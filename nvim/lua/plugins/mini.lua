2a52cbb883074b573dc28819b5e784c484b2eae7
Blake Rain <blake.rain@blakerain.com>
2022-12-29 17:03:15 +0000

    Add some more bits and pieces

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -6,6 +6,39 @@ local M = {
   event = "VeryLazy",
 }
 
+function M.setup_animate()
+  local mouse_scrolled = false
+  for _, scroll in ipairs({ "Up", "Down" }) do
+    local key = "<ScrollWheel" .. scroll .. ">"
+    vim.keymap.set("", key, function()
+      mouse_scrolled = true
+      return key
+    end, { remap = true, expr = true })
+  end
+
+  local animate = require("mini.animate")
+  vim.go.winwidth = 20
+  vim.go.winminwidth = 5
+
+  animate.setup({
+    resize = {
+      timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+    },
+    scroll = {
+      timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+      subscroll = animate.gen_subscroll.equal({
+        predicate = function(total_scroll)
+          if mouse_scrolled then
+            mouse_scrolled = false
+            return false
+          end
+          return total_scroll > 1
+        end,
+      }),
+    },
+  })
+end
+
 function M.config()
   require("mini.indentscope").setup({})
 
@@ -29,6 +62,8 @@ function M.config()
   vim.keymap.set("n", "<leader>bz", function()
     require("mini.misc").zoom(0, {})
   end, { desc = "Zoom into buffer" })
+
+  M.setup_animate()
 end
 
 return M
