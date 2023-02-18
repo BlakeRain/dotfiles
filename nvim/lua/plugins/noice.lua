34f271241416ab580bad7c5afb3936c6b5e79a5a
Blake Rain <blake.rain@blakerain.com>
2023-02-18 12:35:01 +0000

    Slight changes to noice configuration around LSP documentation popups

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -14,48 +14,88 @@ local function redirect()
 end
 
 local M = {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-      },
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        lsp = {
+            signature = {
+                -- enabled = false
+            },
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+            },
+            hover = {
+                opts = {
+                    border = {
+                        style = "rounded"
+                    }
+                }
+            }
+        },
+        presets = {
+            bottom_search = true,
+            command_palette = true,
+            long_message_to_split = true,
+        },
     },
-    presets = {
-      bottom_search = true,
-      command_palette = true,
-      long_message_to_split = true,
+    views = {
+        cmdline_popup = {
+            position = {
+                row = 5,
+                col = "50%"
+            },
+            size = {
+                width = 60,
+                height = "auto"
+            }
+        },
+        popupmenu = {
+            relative = "editor",
+            position = {
+                row = 8,
+                col = "50%"
+            },
+            size = {
+                width = 60,
+                height = 10
+            },
+            border = {
+                style = "rounded",
+                padding = { 0, 1 }
+            },
+            win_options = {
+                winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" }
+            }
+        }
     },
-  },
-  keys = {
-    { "<S-Enter>",
-      redirect,
-      mode = "c",
-      desc = "Redirect Cmdline" },
-    { "<leader>nl",
-      function() require("noice").cmd("last") end,
-      desc = "Noice Last Message" },
-    { "<leader>nh",
-      function() require("noice").cmd("history") end,
-      desc = "Noice History" },
-    { "<leader>na",
-      function() require("noice").cmd("all") end,
-      desc = "Noice All" },
-    { "<c-f>",
-      scroll_forward,
-      silent = true,
-      expr = true,
-      desc = "Scroll forward",
-      mode = { "i", "n", "s" } },
-    { "<c-b>",
-      scroll_backward,
-      silent = true,
-      expr = true,
-      desc = "Scroll backward",
-      mode = { "i", "n", "s" } },
-  },
+    keys = {
+        { "<S-Enter>",
+            redirect,
+            mode = "c",
+            desc = "Redirect Cmdline" },
+        { "<leader>nl",
+            function() require("noice").cmd("last") end,
+            desc = "Noice Last Message" },
+        { "<leader>nh",
+            function() require("noice").cmd("history") end,
+            desc = "Noice History" },
+        { "<leader>na",
+            function() require("noice").cmd("all") end,
+            desc = "Noice All" },
+        { "<c-f>",
+            scroll_forward,
+            silent = true,
+            expr = true,
+            desc = "Scroll forward",
+            mode = { "i", "n", "s" } },
+        { "<c-b>",
+            scroll_backward,
+            silent = true,
+            expr = true,
+            desc = "Scroll backward",
+            mode = { "i", "n", "s" } },
+    },
 }
 
 return M
