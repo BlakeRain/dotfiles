6a1128729dcaee03f32cd44284ef033f26e1f5bf
Blake Rain <blake.rain@blakerain.com>
2023-07-26 15:00:01 +0100

    Automatic update
    Signed-off-by: Blake Rain <blake.rain@blakerain.com>

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -3,3 +3,7 @@ function zsh_stats() {
     | awk '{ CMD[$2]++; count++; } END { for (a in CMD) print CMD[a] " " CMD[a]*100/count "% " a }' \
     | grep -v "./" | sort -nr | head -n 20 | column -c3 -s " " -t | nl
 }
+
+function new_fernet_key() {
+  python3 -c "import random; import base64; print(base64.urlsafe_b64encode(bytes(random.choices(range(255), k=32))).decode('utf-8'))"
+}
