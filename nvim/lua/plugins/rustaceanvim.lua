e1ade6b392e603ee770def42345aae5749f6cd7a
Blake Rain <blakerain@Blakes-MacBook-Pro.local>
2024-01-04 09:51:49 +0000

    Add Rustaceanvim

--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -7,4 +7,29 @@ local M = {
   ft = { "rust" }
 }
 
+vim.g.rustaceanvim = function()
+  local lsp = require("plugins.lsp")
+
+  return {
+    server = {
+      on_attach = lsp.on_attach,
+      settings = {
+        ["rust-analyzer"] = {
+          cargo = {
+            allFeatures = true,
+          },
+          check = {
+            enable = true,
+            command = "clippy",
+            features = "all"
+          },
+          procMacro = {
+            enable = true
+          }
+        }
+      }
+    }
+  }
+end
+
 return M
