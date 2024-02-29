8a99ae17fd673b379ef41ede725399a5ba986d32
Blake Rain <blake.rain@blakerain.com>
2024-02-29 12:31:35 +0000

    Update luasnip keybindings

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -230,17 +230,23 @@ function M.config()
   }))
   })
 
-  vim.keymap.set({ "i", "s" }, "<c-'>", function()
-    if ls.expand_or_jumpable() then ls.expand_or_jump() end
+  vim.keymap.set({ "i" }, "<C-K>", function()
+    ls.expand()
   end, { silent = true })
 
-  vim.keymap.set({ "i", "s" }, "<c-m>",
-    function() if ls.jumpable(-1) then ls.jump(-1) end end,
-    { silent = true })
+  vim.keymap.set({ "i", "s" }, "<C-L>", function()
+    ls.jump(1)
+  end, { silent = true })
 
-  vim.keymap.set({ "i", "s" }, "<c-l>", function()
-    if ls.choice_active() then ls.change_choice(1) end
-  end)
+  vim.keymap.set({ "i", "s" }, "<C-J>", function()
+    ls.jump(-1)
+  end, { silent = true })
+
+  vim.keymap.set({ "i", "s" }, "<C-E>", function()
+    if ls.choice_active() then
+      ls.change_choice(1)
+    end
+  end, { silent = true })
 end
 
 return M
