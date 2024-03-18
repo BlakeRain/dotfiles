3ba7e4167c8203d98f0b1ca71f220a7edc507d50
Blake Rain <blake.rain@blakerain.com>
2024-03-18 14:47:05 +0000

    Add 'neogit' and change '<leader>gg' command

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,19 @@
+return {
+  "NeogitOrg/neogit",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim"
+  },
+  config = true,
+  keys = {
+    {
+      "<leader>gg",
+      function()
+        require("neogit").open()
+      end,
+      desc = "Open NeoGit"
+    }
+  }
+}
