0199d3150dc8b77565e82a4acb6d9d8654eab4ae
Blake Rain <blake.rain@blakerain.com>
2024-06-21 12:08:24 +0100

    feat(ft): add some surrounds for groff

--- nvim/after/ftplugin/groff.lua
+++ nvim/after/ftplugin/groff.lua
@@ -1,2 +1,22 @@
 -- Set the comment string for roff
 vim.bo.commentstring = ".\\\" %s"
+
+require("nvim-surround").buffer_setup({
+  surrounds = {
+    ['I'] = {
+      add = { "\\fI", "\\fP" },
+      find = "\\fI.-\\fP",
+      delete = "^(\\fI)().-(\\fP)()$",
+    },
+    ['B'] = {
+      add = { "\\fB", "\\fP" },
+      find = "\\fB.-\\fP",
+      delete = "^(\\fB)().-(\\fP)()$",
+    },
+    ['C'] = {
+      add = { "\\fC", "\\fP" },
+      find = "\\fC.-\\fP",
+      delete = "^(\\fC)().-(\\fP)()$",
+    }
+  }
+})
