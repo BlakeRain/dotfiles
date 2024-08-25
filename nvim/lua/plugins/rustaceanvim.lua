4594fa85bf4217e6a0eae1d556f34a2609327964
Blake Rain <blake.rain@blakerain.com>
2024-08-25 09:27:34 +0100

    feat: load local `rust-analyzer.json` config file

--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -13,21 +13,35 @@ vim.g.rustaceanvim = function()
   return {
     server = {
       on_attach = lsp.on_attach,
-      settings = {
-        ["rust-analyzer"] = {
-          cargo = {
-            allFeatures = true,
-          },
-          check = {
-            enable = true,
-            command = "clippy",
-            features = "all"
-          },
-          procMacro = {
-            enable = true
-          }
-        }
-      }
+      settings = function(project_root)
+        local ra = require('rustaceanvim.config.server')
+        local settings = ra.load_rust_analyzer_settings(project_root, {
+          settings_file_pattern = 'rust-analyzer.json'
+        })
+
+        return settings
+      end
+
+      -- settings = {
+      --   ["rust-analyzer"] = {
+      --     cargo = {
+      --       allFeatures = true,
+      --     },
+      --     check = {
+      --       enable = true,
+      --       command = "clippy",
+      --       features = "all"
+      --     },
+      --     procMacro = {
+      --       enable = true
+      --     },
+      --     files = {
+      --       excludeDirs = {
+      --         ["**/node_modules"] = true,
+      --       }
+      --     }
+      --   }
+      -- }
     }
   }
 end
