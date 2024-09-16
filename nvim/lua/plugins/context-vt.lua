623bc5248dc2f410bc65e6bcf0079e7c186170b7
Blake Rain <blake.rain@blakerain.com>
2024-09-16 11:51:19 +0100

    fix: add correct prefix character to context VT

--- nvim/lua/plugins/context-vt.lua
+++ nvim/lua/plugins/context-vt.lua
@@ -1,7 +1,11 @@
+-- Virtual text context for Neovim.
+-- https://github.com/andersevenrud/nvim_context_vt
+
 return {
   "andersevenrud/nvim_context_vt",
   event = "VeryLazy",
   opts = {
     enabled = true,
+    prefix = '',
   }
 }
