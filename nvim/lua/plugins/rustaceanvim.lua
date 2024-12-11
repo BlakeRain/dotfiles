49752ddeeedee2a1d7c1f41f3b412b37aab19363
Blake Rain <blake.rain@blakerain.com>
2024-12-11 10:58:00 +0000

    chore: bump version of rustaceanvim and adjust configuration

--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -3,47 +3,16 @@
 
 local M = {
   "mrcjkb/rustaceanvim",
-  version = "^4",
+  version = "^5",
   lazy = false,
 }
 
-vim.g.rustaceanvim = function()
-  local lsp = require("plugins.lsp")
-
-  return {
-    server = {
-      on_attach = lsp.on_attach,
-      settings = function(project_root)
-        local ra = require('rustaceanvim.config.server')
-        local settings = ra.load_rust_analyzer_settings(project_root, {
-          settings_file_pattern = 'rust-analyzer.json'
-        })
-
-        return settings
-      end
-
-      -- settings = {
-      --   ["rust-analyzer"] = {
-      --     cargo = {
-      --       allFeatures = true,
-      --     },
-      --     check = {
-      --       enable = true,
-      --       command = "clippy",
-      --       features = "all"
-      --     },
-      --     procMacro = {
-      --       enable = true
-      --     },
-      --     files = {
-      --       excludeDirs = {
-      --         ["**/node_modules"] = true,
-      --       }
-      --     }
-      --   }
-      -- }
-    }
+vim.g.rustaceanvim = {
+  server = {
+    on_attach = function(client, bufnr)
+      require("plugins.lsp").on_attach(client, bufnr)
+    end
   }
-end
+}
 
 return M
