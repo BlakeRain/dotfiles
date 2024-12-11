7fef8674306d6979194e7ce11dde83c2b032f18e
Blake Rain <blake.rain@blakerain.com>
2024-12-11 16:27:13 +0000

    feat: replace 'live_grep' with a custom grep and use the ivy theme

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -23,7 +23,6 @@ local M = {
     "debugloop/telescope-undo.nvim",
   },
   keys = {
-
     { "<leader>gc",  "<cmd>Telescope git_commits<cr>",               desc = "Show git commits" },
     { "<leader>gb",  "<cmd>Telescope git_bcommits<cr>",              desc = "Show git buffer commits" },
     { "<leader>gs",  "<cmd>Telescope git_status<cr>",                desc = "Show git status" },
@@ -33,7 +32,7 @@ local M = {
     { "<leader>fD",  "<cmd>Telescope diagnostics<cr>",               desc = "Filter diagnostics" },
     { "<leader>ff",  "<cmd>Telescope find_files<cr>",                desc = "Filter files" },
     { "<leader>fF",  "<cmd>Telescope oldfiles<cr>",                  desc = "Filter recent files" },
-    { "<leader>fg",  "<cmd>Telescope live_grep<cr>",                 desc = "Live grep" },
+    -- { "<leader>fg",  "<cmd>Telescope live_grep<cr>",                 desc = "Live grep" },
     { "<leader>fG",  "<cmd>Telescope grep_string<cr>",               desc = "Live grep string" },
     { "<leader>fh",  "<cmd>Telescope help_tags<cr>",                 desc = "Help tags" },
     { "<leader>fHc", "<cmd>Cheatsheet<cr>",                          desc = "Cheatsheet" },
@@ -74,26 +73,87 @@ function M.config()
       }
     },
     pickers = {
+      find_files = {
+        theme = "ivy",
+        prompt_prefix = "🔍 ",
+      },
       notify = {
         layout_strategy = "vertical"
       },
       help_tags = { layout_config = { preview_width = 0.7 } },
       live_grep = {
-        layout_strategy = "vertical",
-        layout_config = {
-          width = 0.9,
-          height = 0.9,
-          preview_height = 0.6,
-          preview_cutoff = 0
-        }
+        theme = "ivy",
+        prompt_prefix = "🔍 ",
       }
     },
     extensions = {
+      fzf = {
+        fuzzy = true,
+        case_mode = "smart_case",
+        override_generic_sorter = true,
+        override_file_sorter = true,
+      }
     }
   })
 
   telescope.load_extension("attempt")
+  telescope.load_extension("fzf")
   telescope.load_extension("undo")
+
+  local live_multigrep = function(opts)
+    opts = opts
+    opts.cwd = opts.cwd or vim.uv.cwd();
+
+    local finder = require("telescope.finders").new_job(function(prompt)
+        if not prompt or prompt == "" then
+          return nil
+        end
+
+        local pieces = vim.split(prompt, "  ")
+        local args = { "rg", "--color=never", "--no-heading", "--with-filename", "--line-number", "--column",
+          "--smart-case" }
+
+        if pieces[1] then
+          table.insert(args, "-e")
+          table.insert(args, pieces[1])
+        end
+
+        if pieces[2] then
+          local arg = pieces[2]
+
+          -- If the argument doesn't look like a glob (it's just letters), then turn it into a glob.
+
+          if arg == '' or arg:match("^[%w]+$") then
+            arg = "*" .. arg .. "*"
+          end
+
+          print("arg", arg)
+
+          table.insert(args, "-g")
+          table.insert(args, arg)
+        end
+
+        return args
+      end,
+      require("telescope.make_entry").gen_from_vimgrep(opts), opts.max_results, opts.cwd)
+
+    require("telescope.pickers").new(opts, {
+      debounce = 100,
+      finder = finder,
+      prompt_title = "Live Grep",
+      previewer = require("telescope.config").values.grep_previewer(opts),
+      sorter = require("telescope.sorters").highlighter_only(opts),
+      attach_mappings = function(_, map)
+        map("i", "<c-space>", require("telescope.actions").to_fuzzy_refine)
+        return true
+      end,
+      push_cursor_on_edit = true,
+    }):find()
+  end
+
+  vim.keymap.set("n", "<leader>fg", function()
+    live_multigrep(require("telescope.themes").get_ivy({}))
+  end, { desc = "Live grep" })
 
   -- Solution for telescope opening file in insert mode
   -- See https://github.com/nvim-telescope/telescope.nvim/issues/2501#issuecomment-1561838990
@@ -104,6 +164,18 @@ function M.config()
       end
     end
   })
+
+  vim.keymap.set("n", "<leader>en", function()
+    require("telescope.builtin").find_files {
+      cwd = vim.fn.stdpath("config")
+    }
+  end)
+
+  vim.keymap.set("n", "<leader>ep", function()
+    require("telescope.builtin").find_files {
+      cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+    }
+  end)
 end
 
 return M
