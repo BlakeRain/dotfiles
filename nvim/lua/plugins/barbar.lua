94ae7063fb9eac9c933853700e549b980bae5703
Blake Rain <blake.rain@blakerain.com>
2024-12-28 22:51:05 +0000

    feat: enable tabbar

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -7,44 +7,45 @@ local M = {
 
   keys = {
     -- Move to previous or next buffer
-    { "<A-,>", "<cmd>BufferPrevious<cr>" },
-    { "<A-.>", "<cmd>BufferNext<cr>" },
+    { "<A-,>",      "<cmd>BufferPrevious<cr>" },
+    { "<A-.>",      "<cmd>BufferNext<cr>" },
 
     -- Re-order current buffer
-    { "<A-<>", "<cmd>BufferMovePrevious<cr>" },
-    { "<A->>", "<cmd>BufferMoveNext<cr>" },
+    { "<A-<>",      "<cmd>BufferMovePrevious<cr>" },
+    { "<A->>",      "<cmd>BufferMoveNext<cr>" },
 
     -- Goto buffer in position
-    { "<A-1>", "<cmd>BufferGoto 1<cr>" },
+    { "<A-1>",      "<cmd>BufferGoto 1<cr>" },
 
     -- Pin or unpin a buffer
-    { "<A-p>", "<cmd>BufferPin<cr>" },
+    { "<A-p>",      "<cmd>BufferPin<cr>" },
 
     -- Close buffer
-    { "<A-c>", "<cmd>BufferClose<cr>" },
+    { "<A-c>",      "<cmd>BufferClose<cr>" },
 
     -- Magic buffer-picking mode
-    { "<C-s>", "<cmd>BufferPick<cr>" },
+    { "<C-s>",      "<cmd>BufferPick<cr>" },
 
-    { "<leader>bb", "<cmd>BufferPick<cr>", desc = "Pick buffer" },
-    { "<leader>bc", "<cmd>BufferClose<cr>", desc = "Close buffer" },
-    { "<leader>bC", "<cmd>BufferClose!<cr>", desc = "Close buffer (force)" },
+    { "<leader>bb", "<cmd>BufferPick<cr>",                       desc = "Pick buffer" },
+    { "<leader>bc", "<cmd>BufferClose<cr>",                      desc = "Close buffer" },
+    { "<leader>bC", "<cmd>BufferClose!<cr>",                     desc = "Close buffer (force)" },
     { "<leader>bx", "<cmd>BufferCloseAllButCurrentOrPinned<cr>", desc = "Close all buffers" },
 
     -- Move to previous or next buffer
-    { "<leader>bn", "<cmd>BufferNext<cr>", desc = "Next buffer" },
-    { "<leader>bp", "<cmd>BufferPrevious<cr>", desc = "Previous buffer" },
+    { "<leader>bn", "<cmd>BufferNext<cr>",                       desc = "Next buffer" },
+    { "<leader>bp", "<cmd>BufferPrevious<cr>",                   desc = "Previous buffer" },
 
     -- Pin or unpin a buffer
-    { "<leader>bP", "<cmd>BufferPin<cr>", desc = "Pin or unpin buffer" },
+    { "<leader>bP", "<cmd>BufferPin<cr>",                        desc = "Pin or unpin buffer" },
+  },
+
+  opts = {
+    tabpages = true
   }
 }
 
 for i = 1, 9 do
   table.insert(M.keys, { ("<A-%i>"):format(i), ("<cmd>BufferGoto %i<cr>"):format(i) })
-end
-
-function M.config()
 end
 
 return M
