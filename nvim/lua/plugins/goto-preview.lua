482f3d174534c8ee08f847a5f1373979bd06a919
Blake Rain <blake.rain@blakerain.com>
2025-03-08 21:53:57 +0000

    fix: missing dependency of 'goto-preview'

--- nvim/lua/plugins/goto-preview.lua
+++ nvim/lua/plugins/goto-preview.lua
@@ -3,13 +3,14 @@
 --
 local M = {
   'rmagatti/goto-preview',
+  dependencies = { "rmagatti/logger.nvim" },
   config = true,
   event = { "BufRead", "BufNewFile" },
   keys = {
-    { "gpd", function() require("goto-preview").goto_preview_definition() end, desc = "Preview definition" },
+    { "gpd", function() require("goto-preview").goto_preview_definition() end,     desc = "Preview definition" },
     { "gpi", function() require("goto-preview").goto_preview_implementation() end, desc = "Preview implementation" },
-    { "gpr", function() require("goto-preview").goto_preview_references() end, desc = "Preview references" },
-    { "gP", function() require("goto-preview").close_all_win() end, desc = "Close preview windows" },
+    { "gpr", function() require("goto-preview").goto_preview_references() end,     desc = "Preview references" },
+    { "gP",  function() require("goto-preview").close_all_win() end,               desc = "Close preview windows" },
   }
 }
 
