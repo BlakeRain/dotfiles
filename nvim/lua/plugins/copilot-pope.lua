53e3ea7bd47c36d106ca053ef2cb895be9dbdc19
Blake Rain <blake.rain@blakerain.com>
2025-07-12 15:00:00 +0100

    Automatic update
    Signed-off-by: Blake Rain <blake.rain@blakerain.com>

--- nvim/lua/plugins/copilot-pope.lua
+++ nvim/lua/plugins/copilot-pope.lua
@@ -3,4 +3,12 @@ local M = {
   lazy = false
 }
 
+-- Do not map tab to accept suggestions
+vim.g.copilot_no_tab_map = true
+
+vim.keymap.set('i', '<C-A>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+
 return M
